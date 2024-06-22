9ba514a9e70c5801233a836371949d74bb42a45f
StephenZR03 <steveoncaffeine@gmail.com>
2024-06-22 19:20:31 +0100

    add input validation and repeat-move checking
    if a player plays in a cell that is not empty the move is rejected. and
        if an input is not specifically a pair of comma-separated numbers
            where the numbers are between the range of 0 to 2, the move is
            rejected.

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ function initBoard() {
     }
 
     const placeToken = (cell, player) => {
-        if (cell.getValue() === null) cell.fill(player);
-        else console.log('this spot is taken');
+        if (cell.getValue() === null) {
+            cell.fill(player);
+            return true;
+        }
+        else return false;
     }
     const getBoard = () => board;
     const getCell = (x, y) => board[x][y];
@@ -46,8 +49,7 @@ function createPlayer(name, token) {
     let currentPlayer = p1;
     let currentCell = null;
 
-    function checkWin(board) {
-        console.log(board.getCell(0, 0) !== null && ((board.getCell(0, 0).getValue() === board.getCell(0, 1).getValue()) && board.getCell(0, 0).getValue() === board.getCell(0, 2).getValue()));
+    const checkWin = (board) => {
         gameOver = true;
         for (let i = 0; i < 3; i++) {
             if (board.getCell(0, 0).getValue() !== null) {
@@ -87,14 +89,36 @@ function createPlayer(name, token) {
         return false;
     }
 
-    // TODO: Stop player from playing in already filled spot
+    const validateInput = (input) => {
+        input = input.split(',').map(i => Number(i));
+        if (input.length !== 2) return false;
+        for (let i = 0; i < input.length; i++) {
+            if (typeof input[i] !== 'number') return false;
+            else if (input[i] > 2 || input[i] < 0) return false;
+        }
+        return true;
+    }
+
+    // TODO: Stop player from playing in already filled spot DONE
     // TODO: Input Validation
-    while (!gameOver) {
+    while (!gameOver) { // game loop
         alert(`It is ${currentPlayer.name}'s turn...`);
         move = prompt("pick a cell to place your marker(x,y): ");
+
+        if (!validateInput(move)) {
+            alert("invalid input, enter a pair of numbers \n(x,y : 0 <= x/y <= 2): ");
+            continue;
+        }
+
         move = move.split(',').map(i => Number(i));
         currentCell = board.getCell(move[0], move[1]);
-        board.placeToken(currentCell, currentPlayer);
+        let placement = board.placeToken(currentCell, currentPlayer);
+
+        if (!placement) {
+            alert("you can't play there pal...");
+            continue;
+        }
+
         if (checkWin(board)) {
             console.log(`${currentPlayer.name} won the game!`);
         } else if (gameOver === true) {
